72be6fa9d0a181f72ca3077ee9b72146b3e6a6e9
Jonas <jonas@fortalezatec.com.br>
2023-12-07 21:59:35 -0300

    Definição da tela de envio de pix

--- src/screens/SendPixScreen.tsx
+++ src/screens/SendPixScreen.tsx
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { theme } from "../themes/Theme";
 import { Feather } from "@expo/vector-icons";
 import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../contexts/AppContext";
 import { Picker } from '@react-native-picker/picker';
+import { SendProps } from "../types/send.t";
 
 export const SendPixScreen = ({ navigation }) => {
 
     const { user, balance, listUsers, users } = useContext(AppContext);
-    const [recipientId, setRecipientId] = useState();
+    
+    const [sender, setSender] = useState<SendProps>({} as SendProps);
 
     useEffect(() => {
         listUsers();
@@ -30,20 +32,37 @@ export const SendPixScreen = ({ navigation }) => {
                 </View>
             </View>
 
-            <Picker
-                selectedValue={recipientId}
-                onValueChange={(itemValue, itemIndex) =>
-                    setRecipientId(itemValue)
-                }>
-                {
-                    users.map((item) => {
-                        return (
-                            <Picker.Item label={item.name} value={item.id} />
+            <View style={styles.body}>
+                <Text style={[theme.fontRegular, styles.label]}>Conta:</Text>
+                <Picker
+                    mode='dialog'
+                    selectedValue={sender.recipientId}
+                    onValueChange={(itemValue) =>
+                        setSender({ ...sender, recipientId: itemValue })
+                    }>
+                    {
+                        users.map((item) => {
+                            return (
+                                <Picker.Item label={item.name} value={item.id} />
+                            )
+                        }
                         )
                     }
-                    )
-                }
-            </Picker>
+                </Picker>
+
+
+                <View style={styles.body}>
+                    <Text style={[theme.fontRegular, styles.label]}>Valor:</Text>
+                    <TextInput
+                        style={[styles.textInput, theme.fontRegular]}
+                        keyboardType='numeric'
+                        placeholder="Valor"
+                        value={sender.value ? sender.value.toString() : ''}
+                        onChangeText={value => setSender({...sender, value: value ? Number.parseFloat(value) : 0 })}
+                    />
+
+                </View>
+            </View>
 
         </View>
     );
@@ -55,5 +74,23 @@ const styles = StyleSheet.create({
         width: 32,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    label: {
+        color: '#000',
+        fontSize: 20
+    },
+    body: {
+        paddingHorizontal: 16,
+        marginTop: 8
+    },
+    textInput: {
+        borderWidth: 1,
+        color: '#FFF',
+        borderColor: '#FFF',
+        borderRadius: 8,
+        padding: 8,
+        height: 40,
+        marginTop: 8,
+        width: '100%'
     }
 })
